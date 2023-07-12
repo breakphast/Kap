f4b45738b275d5af94bb4ca48743ce3ec619495b
Desmond Fitch <desmond.fitch@icloud.com>
2023-07-12 18:57:29 -0500

    Initial Commit

---
+++ Kap/KapApp.swift
@@ -0,0 +1,17 @@
+//
+//  KapApp.swift
+//  Kap
+//
+//  Created by Desmond Fitch on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct KapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
