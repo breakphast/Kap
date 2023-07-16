b2094c95e59940bed620fc605308842879585ca1
Desmond Fitch <desmond.fitch@icloud.com>
2023-07-15 23:31:27 -0500

    Bet result function

--- Kap/Models/Bet.swift
+++ Kap/Models/Bet.swift
@@ -30,7 +30,7 @@ struct BetOption {
     var under: Double
     var betString: String
     var selectedTeam: String?
-
+    
     init(gameID: String, betType: BetType, odds: Int, spread: Double? = nil, over: Double, under: Double, selectedTeam: String? = nil) {
         self.id = UUID()
         self.gameID = gameID
@@ -40,9 +40,9 @@ struct BetOption {
         self.over = over
         self.under = under
         self.selectedTeam = selectedTeam
-
+        
         let formattedOdds = odds > 0 ? "+\(odds)" : "\(odds)"
-
+        
         switch betType {
         case .spread:
             if let spread = spread {
@@ -95,7 +95,39 @@ struct Bet {
         } else {
             return 0
         }
-        
         return bet.result == .win ? Int(round(points)) : Int(round(-points)) / 2
+    }
+    
+    func checkBetResult(bet: Bet) -> BetResult {
+        guard let homeScore = Int(bet.game.homeScore ?? ""), let awayScore = Int(bet.game.awayScore ?? "") else {
+            return .pending
+        }
+        let selectedTeam = bet.game.betOptions.first(where: { $0.id == bet.betOptionID })?.selectedTeam
+        
+        switch bet.type {
+        case .moneyline:
+            guard homeScore != awayScore else { return .push }
+            
+            if selectedTeam == bet.game.homeTeam {
+                return homeScore > awayScore ? .win : .loss
+            } else if selectedTeam == bet.game.awayTeam {
+                return homeScore < awayScore ? .win : .loss
+            }
+        case .spread:
+            let spread = bet.game.betOptions.first(where: { $0.id == bet.betOptionID })?.spread ?? 0.0
+            if selectedTeam == bet.game.homeTeam {
+                return Double(homeScore) + spread > Double(awayScore) ? .win : .loss
+            } else if selectedTeam == bet.game.awayTeam {
+                return Double(awayScore) + spread > Double(homeScore) ? .win : .loss
+            }
+        case .over:
+            let over = bet.game.betOptions.first(where: { $0.id == bet.betOptionID })?.over ?? 0.0
+            return Double(homeScore + awayScore) > over ? .win : .loss
+        case .under:
+            let under = bet.game.betOptions.first(where: { $0.id == bet.betOptionID })?.under ?? 0.0
+            return Double(homeScore + awayScore) < under ? .win : .loss
+        }
+        
+        return .pending
     }
 }
